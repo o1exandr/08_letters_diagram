619bc102a42e1d5fdce8fae2ccafd55ba1a37805
olexandr.finyuk <olexandr.finyuk@gmail.com>
2018-09-25 19:03:55 +0300

    done

--- 08_letters_diagram/Program.cs
+++ 08_letters_diagram/Program.cs
@@ -1,15 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/*
+ Завдання 8. Дано текст. Вивести статистику  зустрічі у тексті лат букв у вигляді гістограми
+(цикл по буквах алфавіту, Count(лямбда))
+ */
+
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _08_letters_diagram
 {
     class Program
     {
+        static void Gistoram(string text)
+        {
+            int cnt;
+            Console.WriteLine(text);
+            for (int i = 97; i < 123; i++)
+            {
+                cnt = text.Count(x => (x == Convert.ToChar(i)) || x == Convert.ToChar(i - 32));
+                if (cnt != 0)
+                { 
+                    Console.Write($"{Convert.ToChar(i)}: {cnt}\t");
+                    for (int k = 0; k < cnt; k++)
+                        Console.Write("|");
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
+            //Gistoram("The quick brown fox jumps over the lazy dog");
+            Gistoram("The grass is always greener on the other side of the fence");          
         }
     }
 }
